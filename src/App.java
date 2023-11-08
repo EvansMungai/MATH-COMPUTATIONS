8d303db833df88d3d18f691bf57922a447daaa48
Evans Mungai <thetracker254@gmail.com>
2023-11-08 11:04:11 +0300

    Create Circle Perimeter class

--- src/App.java
+++ src/App.java
@@ -12,6 +12,14 @@ class TriangleArea {
         return area;
     }
 }
+class CirclePerimeter {
+    public double circlePerimeter(double radius){
+        final double pi = 3.14159265359;
+        double perimeter = 2 * pi * radius;
+        System.out.println("The perimeter of the cirlce of radius "+ radius+ " is "+perimeter);
+        return perimeter;
+    }
+}
 public class App {
     public static void main(String[] args) throws Exception {
         Addition addition = new Addition();
